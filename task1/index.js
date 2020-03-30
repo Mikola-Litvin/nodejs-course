9dbcc1b3fa72eca8fbae0e55f26ac31ebeb66379
Nikolai Shkrabkov <grr_by@mail.ru>
2020-03-30 23:50:49 +0300

    feat: streams and pipeline

--- task1/index.js
+++ task1/index.js
@@ -1,4 +1,8 @@
-const mycli = require('commander')
+const mycli = require('commander');
+const { pipeline } = require('stream');
+const fs = require('fs');
+const DIR_PATH = './task1/';
+let isFile;
 
 mycli
   .option('-s, --shift <shift>', `a shift`)
@@ -10,10 +14,46 @@ mycli
       if (!mycli.shift || !mycli.actions) {
         process.stderr.write("You should pass all mandatory parameters\n");
       } else {
-        console.log(mycli.shift);
-        console.log(mycli.actions);
+        let inpt;
+        let outpt;
+        if (!mycli.input) {
+          inpt = process.stdin;
+        } else {
+          inpt = fs.createReadStream(`${DIR_PATH}${mycli.input}`);
+          inpt.on(
+            'error',
+            () => {
+              process.stderr.write(`${mycli.input} does not exist.\n`);
+            }
+          )
+        }
+        if (!mycli.output) {
+          outpt = process.stdout;
+        } else {
+          outpt = fs.createWriteStream(`${DIR_PATH}${mycli.output}`);
+        }
+        pipeline(
+          inpt,
+          outpt,
+          (err) => {
+            if (err) {
+              return
+            } 
+          }
+        );
       }
     }
   })
+
+  const encodeToRot13 = (str) => {
+    const str1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const str2 = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+    const arr = [];
+    for (let i = 0; i < str.length; i += 1) {
+      const index = str1.indexOf(str[i]);
+      arr.push(str2[index]);
+    }
+    return arr.join('');
+  }
 
 mycli.parse(process.argv)
